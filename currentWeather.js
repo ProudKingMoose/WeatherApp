70a138f0c9fe8597a30127b580e77636f842b5cf
ProudKingMoose <mose.villasmil@hotmail.se>
2023-10-26 13:40:34 +0200

    KAKKAKKAKAKAA

--- currentWeather.js
+++ currentWeather.js
@@ -50,7 +50,7 @@ if ("geolocation" in navigator) {
     console.log(longitude);
     console.log(latitude);
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weathercode,windspeed_10m,winddirection_10m,windgusts_10m&windspeed_unit=ms&forecast_days=1",{
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weathercode,cloudcover,windspeed_10m,winddirection_10m,windgusts_10m&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,windspeed_10m,winddirection_10m,windgusts_10m&forecast_days=14",{
         method:'GET'
     }).then(response => {
         if (!response.ok){
@@ -63,14 +63,35 @@ if ("geolocation" in navigator) {
         var temp = result.current.temperature_2m;
         var windspeed = result.current.windspeed_10m;
         var windgusts = result.current.windgusts_10m;
+        var precipitation = result.current.precipitation;
         var winddirection = degToCompass(result.current.winddirection_10m);
         document.getElementById("temp").innerText = temp + "C°"
         document.getElementById("wSpeed").innerText = windspeed + "m/s    " + winddirection;
+        document.getElementById("percip").innerText = precipitation + " mm";
         console.log(temp);
         console.log(windspeed);
         console.log(windgusts);
         console.log(winddirection);
+        getWcode(result.current.weathercode);
     })
+}
+
+function getWcode(wCode){
+fetch("https://gist.githubusercontent.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c/raw/76b0cb0ef0bfd8a2ec988aa54e30ecd1b483495d/descriptions.json",{
+    method:"GET"
+}).then(response => {
+    if (!response.ok){
+        throw new Error("Network response didn't work");
+    }
+    return response.json();
+})
+.then(result =>{
+    console.log(result);
+    var dsc = result[wCode].day.description;
+    var img = result[wCode].day.image;
+    document.getElementById("cwpt").innerText = dsc;
+    document.getElementById("pwdsc").src = img;
+})
 }
 
 function degToCompass(num) {
--- main.js
+++ main.js
@@ -34,38 +34,21 @@ function GetDailyData(longitude, latitude){
     console.log(longitude);
     console.log(latitude);
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m",{
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weathercode,cloudcover,windspeed_10m,winddirection_10m,windgusts_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&windspeed_unit=ms&timezone=auto&forecast_days=14",{
         method:'GET'
     }).then(response => {
         if (!response.ok){
             throw new Error("Network response didn't work");
-        }
+        }   
         return response.json();
     })
     .then(result => {
         console.log(result);
-        var temp = result.current_weather.temperature;
-        console.log(temp);
-        document.getElementById("temperatureText").innerText = "Current Temperature: " + temp + "°C";
-    })
-}
+        for(var i = 0; i < 14; i++){
 
-function GetHourlyData(longitude, latitude){
-    console.log(longitude);
-    console.log(latitude);
-
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,visibility,windspeed_10m,windspeed_80m,windspeed_120m,winddirection_10m,winddirection_80m,winddirection_120m,windgusts_10m,temperature_80m,temperature_120m&forecast_days=1",{
-        method:'GET'
-    }).then(response => {
-        if (!response.ok){
-            throw new Error("Network response didn't work");
+            for (var i = 0; i < 24; i++){
+                
+            }
         }
-        return response.json();
-    })
-    .then(result => {
-        console.log(result);
-        var temp = result.current_weather.temperature;
-        console.log(temp);
-        document.getElementById("temperatureText").innerText = "Current Temperature: " + temp + "°C";
     })
 }
